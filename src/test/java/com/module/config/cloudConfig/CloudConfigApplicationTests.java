1431aaf4e46349d8911cbd70f18acf0873f3d5cd
kowshik bharathi <kowshikbharathim@gmail.com>
2020-11-21 15:52:56 +0530

    Initial commit

---
+++ src/test/java/com/module/config/cloudConfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.config.cloudConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
